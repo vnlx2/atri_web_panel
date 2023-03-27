02c888389f2026329adc3a788fb08d1c355a7699
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-03-27 23:19:12 +0700

    VisualNovel: Remove padding between Header and add button
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- src/views/admin/VisualNovel.vue
+++ src/views/admin/VisualNovel.vue
@@ -83,8 +83,7 @@ function showSnackbarStatus(response) {
 <template>
 	<HeaderName>Visual Novel's List</HeaderName>
 	<div class="mt-10 p-5">
-
-		<div class="pt-6 flex justify-between">
+		<div class="flex justify-between">
 			<div class="basis-3/4">
 				<button @click="add" type="button"
 					class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
