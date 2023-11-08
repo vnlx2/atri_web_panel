b17d3bd6ccd64d6ce4526eff3d2b4c7f3c83d5d6
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-11-08 14:57:36 +0700

    Dashboard: Show number of VN in vndb and db
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -9,5 +9,21 @@ definePageMeta({
 </script>
 
 <template>
-    <p>Dashboard</p>
+    <div class="px-8 py-14 w-full h-full">
+        <h1 class="font-sans font-medium text-3xl my-5 rounded-lg">Dashboard</h1>
+        <div class="grid grid-cols-3 gap-x-5">
+            <div class="border flex flex-col p-6 bg-white">
+                <span>Number of VN in VNDB</span>
+                <p class="text-3xl py-3 font-semibold">100</p>
+            </div>
+            <div class="border flex flex-col p-6 bg-white">
+                <span>Number of VN in Database</span>
+                <p class="text-3xl py-3 font-semibold">100</p>
+            </div>
+            <div class="border flex flex-col p-6 bg-white">
+                <span>Number of VN in Database with URL</span>
+                <p class="text-3xl py-3 font-semibold">100</p>
+            </div>
+        </div>
+    </div>
 </template>
