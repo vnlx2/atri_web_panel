e33da82e7ce9a6006987d2e68bba7acc89d97b85
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-11-14 19:13:47 +0700

    VisualNovel: Add Pagination and table
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

---
+++ components/Pagination.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+  const props = defineProps({
+    itemsCount: Number,
+    itemsPerPage: Number,
+    currentPage: Number,
+  });
+  const emits = defineEmits(['changePage']);
+  const pageCount = computed(() =>
+    Math.ceil((props.itemsCount?? 1) / (props.itemsPerPage?? 1))
+  );
+  const minimumNumber = computed(() => 
+    (Math.ceil((props.currentPage ?? 1) / 5) * 5) - 5
+  );
+  console.log(minimumNumber.value);
+
+  function previous() {
+    if (props.currentPage! > 1) {
+      emits('changePage', props.currentPage! - 1);
+    }
+  }
+
+  function next() {
+    if (props.currentPage! < pageCount.value) {
+      emits('changePage', props.currentPage! + 1);
+    }
+  }
+</script>
+
+<template>
+  <div class="flex justify-between py-5">
+    <p class="text-gray-700 text-sm">Page {{ props.currentPage }} of {{ pageCount }}</p>
+    <nav aria-label="Page navigation example">
+      <ul class="flex items-center -space-x-px h-8 text-sm">
+        <li>
+          <a :class="['flex items-center justify-center px-3 h-8 ml-0 leading-tight rounded-l-lg', currentPage === 1 ? 'text-gray-700 bg-gray-300 cursor-not-allowed' : 'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700']" @click="previous"
+          >
+            <span class="sr-only">Previous</span>
+            <svg
+              class="w-2.5 h-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+          </a>
+        </li>
+        <li 
+          v-for="index in 5" 
+          @click="$emit('changePage', index + minimumNumber)" 
+          :class="['flex items-center justify-center px-3 h-8 leading-tight', currentPage === (index + minimumNumber) ? 'text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700' : 'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700']">{{ index + minimumNumber }}</li>
+        <li>
+          <a
+          :class="['flex items-center justify-center px-3 h-8 ml-0 leading-tight rounded-r-lg', currentPage === pageCount ? 'text-gray-700 bg-gray-300 cursor-not-allowed' : 'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700']" @click="next"
+          >
+            <span class="sr-only">Next</span>
+            <svg
+              class="w-2.5 h-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
--- pages/admin/visualnovel.vue
+++ pages/admin/visualnovel.vue
@@ -1,9 +1,122 @@
-<script setup>
+<script setup lang="ts">
+const currentPage = ref(1);
+const itemsPerPage = ref(10);
+const isLoading = ref(false);
 
+function changePage(pageNumber: number) {
+  currentPage.value = pageNumber;
+}
 </script>
 
 <template>
-    <div class="px-8 py-14 w-full h-full">
-        <h1 class="font-sans font-medium text-3xl my-5 rounded-lg">Visual Novel</h1>
+  <div class="px-8 py-14 w-full h-full">
+    <h1 class="font-sans font-medium text-3xl my-5 rounded-lg">Visual Novel</h1>
+    <div class="bg-white rounded-lg shadow-md px-5">
+      <div class="flex justify-between py-6">
+        <button
+          class="flex items-center bg-blue-600 hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 transition-colors text-white font-medium px-3 py-2 rounded-md shadow-sm"
+        >
+          <i class="ri-add-line text-lg font-bold mr-2"></i>
+          <p>Add New</p>
+        </button>
+        <div class="relative flex items-center">
+          <div
+            class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none"
+          >
+            <i class="ri-search-2-line"></i>
+          </div>
+          <input
+            type="text"
+            class="border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 pl-10"
+            placeholder="Search"
+          />
+        </div>
+      </div>
+      <div v-if="isLoading" class="py-3">
+        <table class="table-auto animate-pulse w-full">
+          <tbody>
+            <tr class="border-gray-300" v-for="items in 5">
+              <td class="py-2 text-left pl-5 w-1/4">
+                <div class="bg-gray-300 text-transparent rounded-lg">
+                  s
+                </div>
+              </td>
+              <td class="py-2 text-left pl-5 w-2/4">
+                <div class="bg-gray-300 text-transparent rounded-lg">
+                  a
+                </div>
+              </td>
+              <td class="py-2 text-left pl-5 w-1/4">
+                <div class="bg-gray-300 text-transparent rounded-lg w-full">
+                  a
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else class="py-3">
+        <table class="table-auto w-full">
+          <thead>
+            <tr class="border-y border-gray-300">
+              <th class="py-2 text-left pl-5">Code</th>
+              <th class="py-2 text-left pl-5">Title</th>
+              <th class="py-2 text-left pl-5">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-y border-gray-300">
+              <td class="py-2 pl-5">9</td>
+              <td class="py-2 pl-5">Tomoyo After ~It's a Wonderful Life~</td>
+              <td class="py-2 pl-5">
+                <button
+                  class="bg-blue-500 hover:bg-blue-400 focus:ring-3 focus:ring-blue-200 transition-colors text-white font-medium rounded-md shadow-sm px-2.5 mx-1 py-1"
+                >
+                  <i class="ri-eye-line"></i>
+                </button>
+                <button
+                  class="bg-yellow-500 hover:bg-yellow-400 focus:ring-3 focus:ring-yellow-200 transition-colors text-white font-medium rounded-md shadow-sm px-2.5 mx-1 py-1"
+                >
+                  <i class="ri-pencil-line"></i>
+                </button>
+                <button
+                  class="bg-red-500 hover:bg-red-400 focus:ring-3 focus:ring-red-200 transition-colors text-white font-medium rounded-md shadow-sm px-2.5 mx-1 py-1"
+                >
+                  <i class="ri-delete-bin-line"></i>
+                </button>
+              </td>
+            </tr>
+            <tr class="border-y border-gray-300">
+              <td class="py-2 pl-5">9</td>
+              <td class="py-2 pl-5">Tomoyo After ~It's a Wonderful Life~</td>
+              <td class="py-2 pl-5">
+                <button
+                  class="bg-blue-500 hover:bg-blue-400 focus:ring-3 focus:ring-blue-200 transition-colors text-white font-medium rounded-md shadow-sm px-2.5 mx-1 py-1"
+                >
+                  <i class="ri-eye-line"></i>
+                </button>
+                <button
+                  class="bg-yellow-500 hover:bg-yellow-400 focus:ring-3 focus:ring-yellow-200 transition-colors text-white font-medium rounded-md shadow-sm px-2.5 mx-1 py-1"
+                >
+                  <i class="ri-pencil-line"></i>
+                </button>
+                <button
+                  class="bg-red-500 hover:bg-red-400 focus:ring-3 focus:ring-red-200 transition-colors text-white font-medium rounded-md shadow-sm px-2.5 mx-1 py-1"
+                >
+                  <i class="ri-delete-bin-line"></i>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <Pagination
+        v-if="!isLoading"
+        :itemsCount="200"
+        :itemsPerPage="itemsPerPage"
+        :currentPage="currentPage"
+        @changePage="changePage"
+      />
     </div>
+  </div>
 </template>
