3831be911ff97a3ae29b8062bdea7acbc13cdc68
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-11-23 00:40:43 +0700

    Dashboard: Show dashboard stats
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,6 +2,14 @@
 useHead({
     title: 'Dashboard',
 });
+
+const vndbTotal = ref(0);
+const dbVNTotal = ref(0);
+const dbVNWithUrlTotal = ref(0);
+
+onBeforeMount(async () => {
+    [vndbTotal.value, dbVNTotal.value, dbVNWithUrlTotal.value] = await getDashboardStats();
+});
 </script>
 
 <template>
@@ -10,15 +18,15 @@ useHead({
         <div class="grid grid-rows-3 gap-y-5 md:grid-cols-3 md:gap-x-5">
             <div class="border flex flex-col p-6 bg-white">
                 <span>Number of VN in VNDB</span>
-                <p class="text-3xl py-3 font-semibold">100</p>
+                <p class="text-3xl py-3 font-semibold">{{ vndbTotal }}</p>
             </div>
             <div class="border flex flex-col p-6 bg-white">
                 <span>Number of VN in Database</span>
-                <p class="text-3xl py-3 font-semibold">100</p>
+                <p class="text-3xl py-3 font-semibold">{{ dbVNTotal }}</p>
             </div>
             <div class="border flex flex-col p-6 bg-white">
                 <span>Number of VN in Database with URL</span>
-                <p class="text-3xl py-3 font-semibold">100</p>
+                <p class="text-3xl py-3 font-semibold">{{ dbVNWithUrlTotal }}</p>
             </div>
         </div>
     </div>
