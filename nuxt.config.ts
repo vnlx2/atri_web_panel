1120ce687beecdaab5175c951ff9c22693cdfe09
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-12-23 00:43:04 +0700

    Package: Add Pinia State management
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
 
   css: ['~/assets/css/main.css'],
   postcss: {
@@ -17,5 +23,5 @@ export default defineNuxtConfig({
     }
   },
 
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@pinia/nuxt"]
 })
