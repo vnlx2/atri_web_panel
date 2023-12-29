f719698a51bfffb15f6c6a5c08d86662b6e30ce4
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-12-29 16:40:21 +0700

    Login: Add enter trigger to execute login button
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- pages/login.vue
+++ pages/login.vue
@@ -124,6 +124,7 @@ async function loginPress() {
             <input
               ref="username"
               @keydown="reset"
+              @keyup.enter="loginPress"
               :class="[
                 errorMessage.username.status.value
                   ? 'focus:border-red-500'
@@ -158,6 +159,7 @@ async function loginPress() {
                   ? 'focus:border-red-500'
                   : 'focus:border-blue-500',
               ]"
+              @keyup.enter="loginPress"
               class="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-md font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-300 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               placeholder=""
               type="password"
